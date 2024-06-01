bace7b2b80603db7f211ab4eb0131123c5dcae12
Proud of JW <154409699+oncliff-climbing@users.noreply.github.com>
2024-06-01 20:48:19 +0900

    Update health.js
    health check test

--- front-app/src/health/health.js
+++ front-app/src/health/health.js
@@ -1,22 +1,56 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// const Health = () => {
+//   const [status, setStatus] = useState('Loading...');
+//   useEffect(() => {
+//     const fetchHealthStatus = async () => {
+//       try {
+//         const response = await axios.get('http://www.cloudeof.com:3000/health');
+//         if (response.status === 200) {
+//           setStatus('Server is healthy');
+//         } else {
+//           setStatus('Server is not healthy');
+//         }
+//       } catch (error) {
+//         setStatus('Server is not healthy');
+//       }
+//     };
+//     fetchHealthStatus();
+//   }, []);
+//   return (
+//     <div className="health">
+//       <h2>Health Check</h2>
+//       <p>{status}</p>
+//     </div>
+//   );
+// }
+// export default Health;
+
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
 const Health = () => {
   const [status, setStatus] = useState('Loading...');
+
   useEffect(() => {
     const fetchHealthStatus = async () => {
       try {
         const response = await axios.get('http://www.cloudeof.com:3000/health');
-        if (response.status === 200) {
+        console.log('Health check response:', response); // 응답 로그 추가
+        if (response.status === 200 && response.data.status === "OK") {
           setStatus('Server is healthy');
         } else {
           setStatus('Server is not healthy');
         }
       } catch (error) {
+        console.error('Health check error:', error); // 에러 로그 추가
         setStatus('Server is not healthy');
       }
     };
     fetchHealthStatus();
   }, []);
+
   return (
     <div className="health">
       <h2>Health Check</h2>
@@ -24,4 +58,5 @@ const Health = () => {
     </div>
   );
 }
+
 export default Health;
